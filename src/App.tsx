5011a2b82768ea7a347bdc089a76fcfac2633d2c
CandyyPL <aplay58.yt@gmail.com>
2025-12-30 15:18:55 +0100

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,7 @@
+const App = () => {
+  return (
+    <h1 className='font-montserrat text-4xl font-semibold'>Hello, World!</h1>
+  );
+};
+
+export default App;
---
+++ src/main.tsx
@@ -0,0 +1,17 @@
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import WebFont from 'webfontloader';
+import '@/assets/global.css';
+import App from '@/App.tsx';
+
+WebFont.load({
+  google: {
+    families: ['Montserrat'],
+  },
+});
+
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+);

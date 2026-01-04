223abc2cb09acb76707f0cc25c842cce124e0fe4
CandyyPL <aplay58.yt@gmail.com>
2026-01-04 12:04:48 +0100

    change font to noto sans

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,5 @@
 const App = () => {
-  return (
-    <h1 className='font-montserrat text-4xl font-semibold'>Hello, World!</h1>
-  );
+  return <h1 className='font-noto text-4xl font-semibold'>Hello, World!</h1>;
 };
 
 export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,7 @@ import App from '@/App.tsx';
 
 WebFont.load({
   google: {
-    families: ['Montserrat'],
+    families: ['Noto Sans'],
   },
 });
 

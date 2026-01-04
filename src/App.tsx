554a7f910de29a93ff06abfa13e0bde3c4d63f45
CandyyPL <aplay58.yt@gmail.com>
2026-01-04 12:18:19 +0100

    add react router

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,17 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Dashboard from '@/pages/Dashboard';
+
 const App = () => {
-  return <h1 className='font-noto text-4xl font-semibold'>Hello, World!</h1>;
+  return (
+    <Router>
+      <Routes>
+        <Route
+          path='/'
+          element={<Dashboard />}
+        />
+      </Routes>
+    </Router>
+  );
 };
 
 export default App;

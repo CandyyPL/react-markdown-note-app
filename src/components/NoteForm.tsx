710589bdb5fd7c89dcfa8d9dc5fa83e1b3d10c6b
CandyyPL <aplay58.yt@gmail.com>
2026-01-12 10:18:10 +0100

    made new note form skeleton

--- src/components/NoteForm.tsx
+++ src/components/NoteForm.tsx
@@ -1,3 +1,4 @@
+import { Button } from '@/components/ui/button';
 import { Field, FieldGroup, FieldLabel, FieldSet } from '@/components/ui/field';
 import { Input } from '@/components/ui/input';
 import {
@@ -9,6 +10,8 @@ import {
   MultiSelectValue,
 } from '@/components/ui/multi-select';
 import { Textarea } from '@/components/ui/textarea';
+import { useRef, type FormEvent } from 'react';
+import { Link } from 'react-router-dom';
 
 type Tag = {
   label: string;
@@ -31,19 +34,29 @@ const tags: Tag[] = [
 ];
 
 const NoteForm = () => {
+  const titleRef = useRef<HTMLInputElement>(null);
+  const markdownRef = useRef<HTMLTextAreaElement>(null);
+
+  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    console.log(titleRef.current?.value);
+    console.log(markdownRef.current?.value);
+  };
+
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <FieldSet>
         <FieldGroup>
           <div className='grid grid-cols-2 gap-4'>
             <Field>
               <FieldLabel className='text-xl'>Title</FieldLabel>
-              <Input />
+              <Input ref={titleRef} />
             </Field>
             <Field>
               <FieldLabel className='text-xl'>Tags</FieldLabel>
               <MultiSelect>
-                <MultiSelectTrigger className='w-full'>
+                <MultiSelectTrigger>
                   <MultiSelectValue placeholder='Select tags...' />
                 </MultiSelectTrigger>
                 <MultiSelectContent search={false}>
@@ -60,7 +73,28 @@ const NoteForm = () => {
           </div>
           <Field>
             <FieldLabel className='text-xl'>Body</FieldLabel>
-            <Textarea className='resize-none' />
+            <Textarea
+              className='resize-none'
+              rows={18}
+              ref={markdownRef}
+            />
+          </Field>
+          <Field
+            orientation='horizontal'
+            className='justify-end'>
+            <Button
+              type='submit'
+              variant='default'
+              className='cursor-pointer'>
+              Save
+            </Button>
+            <Link to='..'>
+              <Button
+                variant='outline'
+                className='cursor-pointer'>
+                Cancel
+              </Button>
+            </Link>
           </Field>
         </FieldGroup>
       </FieldSet>

d2107fc5f179760950729dd6e70e03d909a154db
CandyyPL <aplay58.yt@gmail.com>
2026-01-13 10:22:48 +0100

    add new note button to the dashboard

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,5 +1,15 @@
+import { Button } from '@/components/ui/button';
+import { Link } from 'react-router-dom';
+
 const Dashboard = () => {
-  return <div>Dashboard</div>;
+  return (
+    <main className='p-4'>
+      <h1 className='mb-4 text-4xl font-semibold'>Dashboard</h1>
+      <Link to='/new'>
+        <Button className='cursor-pointer'>Create new note</Button>
+      </Link>
+    </main>
+  );
 };
 
 export default Dashboard;

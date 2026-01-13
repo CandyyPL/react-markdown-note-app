912ea91d1090f665eb48bc2b1bf0e7f41a0b869d
CandyyPL <aplay58.yt@gmail.com>
2026-01-13 12:25:30 +0100

    work in progress

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,13 +1,30 @@
-import { Button } from '@/components/ui/button';
-import { Link } from 'react-router-dom';
+import useLocalStorage from '@/hooks/useLocalStorage';
+import NewNote from '@/pages/NewNote';
+import type { NoteData, Tag } from '@/types/note';
+import { useMemo } from 'react';
+// import { Button } from '@/components/ui/button';
+// import { Link } from 'react-router-dom';
 
 const Dashboard = () => {
+  const [notes, setNotes] = useLocalStorage<NoteData[]>('notes', []);
+  const [tags, setTags] = useLocalStorage<Tag[]>('tags', []);
+
+  const notesWithTags = useMemo(
+    () =>
+      notes.map((note) => ({
+        ...note,
+        tags: tags.filter((tag) => note.tagIds.includes(tag.id)),
+      })),
+    [notes, tags]
+  );
+
   return (
     <main className='p-4'>
       <h1 className='mb-4 text-4xl font-semibold'>Dashboard</h1>
-      <Link to='/new'>
+      {/* <Link to='/new'>
         <Button className='cursor-pointer'>Create new note</Button>
-      </Link>
+      </Link> */}
+      <NewNote />
     </main>
   );
 };

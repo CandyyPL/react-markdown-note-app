282a962c5c564637eada359300fb75c07a43969b
CandyyPL <aplay58.yt@gmail.com>
2026-01-13 11:50:36 +0100

    update note types

--- src/types/note.ts
+++ src/types/note.ts
@@ -5,23 +5,19 @@ export const TagSchema = z.object({
   value: z.string(),
 });
 
-export const FormDataSchema = z.object({
+export const NoteDataSchema = z.object({
   title: z.string().min(3, 'Minimum 3 characters required'),
   body: z.string().min(50, 'Minimum 50 characters required'),
-  tags: z.array(z.string()),
+  tagIds: z.array(z.string()),
 });
 
-export type Tag = z.infer<typeof TagSchema>;
-export type FormData = z.infer<typeof FormDataSchema>;
+export type TagData = z.infer<typeof TagSchema>;
+export type NoteData = z.infer<typeof NoteDataSchema>;
+
+export type Tag = {
+  id: string;
+} & TagData;
 
 export type Note = {
   id: string;
-} & FormData;
-
-export type RawNoteData = Pick<FormData, 'title' | 'body'> & {
-  tagIds: string[];
-};
-
-export type RawNote = {
-  id: string;
-} & RawNoteData;
+} & NoteData;

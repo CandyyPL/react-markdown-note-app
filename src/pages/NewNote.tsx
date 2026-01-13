52ef38bf7150d071c13ea655841e35d135e2fa61
CandyyPL <aplay58.yt@gmail.com>
2026-01-13 12:25:19 +0100

    change main tag to section tag

--- src/pages/NewNote.tsx
+++ src/pages/NewNote.tsx
@@ -2,10 +2,10 @@ import NoteForm from '@/components/NoteForm';
 
 const NewNote = () => {
   return (
-    <main className='container mx-auto my-4 max-w-200'>
+    <section className='container mx-auto my-4 max-w-200'>
       <h1 className='mb-4 text-4xl font-semibold'>Create new note</h1>
       <NoteForm />
-    </main>
+    </section>
   );
 };
 

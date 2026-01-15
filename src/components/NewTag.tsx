bae29a49609de175e97685485c9b1341a623c0dc
CandyyPL <aplay58.yt@gmail.com>
2026-01-15 12:25:34 +0100

    add tag adding logic

--- src/components/NewTag.tsx
+++ src/components/NewTag.tsx
@@ -9,41 +9,99 @@ import {
   DialogFooter,
   DialogClose,
 } from '@/components/ui/dialog';
-import { Field, FieldGroup, FieldLabel, FieldSet } from '@/components/ui/field';
+import {
+  Field,
+  FieldError,
+  FieldGroup,
+  FieldLabel,
+  FieldSet,
+} from '@/components/ui/field';
 import { Input } from '@/components/ui/input';
+import useTags from '@/hooks/useTags';
+import { TagSchema } from '@/types/tag';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { Controller, useForm } from 'react-hook-form';
+import type z from 'zod';
 
 const NewTag = () => {
+  const { onCreateTag } = useTags();
+
+  const form = useForm<z.infer<typeof TagSchema>>({
+    resolver: zodResolver(TagSchema),
+    defaultValues: {
+      label: '',
+      value: '',
+    },
+  });
+
+  const onSubmit = (data: z.infer<typeof TagSchema>) => {
+    form.reset();
+    onCreateTag(data);
+  };
+
   return (
     <Dialog>
-      <form>
-        <DialogTrigger asChild>
-          <Button className='cursor-pointer'>Create new Tag</Button>
-        </DialogTrigger>
-        <DialogContent>
+      <DialogTrigger asChild>
+        <Button className='cursor-pointer'>Create new Tag</Button>
+      </DialogTrigger>
+      <DialogContent>
+        <form
+          onSubmit={form.handleSubmit(onSubmit)}
+          className='flex flex-col gap-4'>
           <DialogHeader>
             <DialogTitle>Create new Tag</DialogTitle>
             <DialogDescription>
-              Enter a title and id for the new Tag.
+              Enter a label and ID for the new Tag.
             </DialogDescription>
           </DialogHeader>
           <FieldSet>
             <FieldGroup>
-              <Field>
-                <FieldLabel>Tag ID</FieldLabel>
-                <Input />
-              </Field>
-              <Field>
-                <FieldLabel>Tag name</FieldLabel>
-                <Input />
-              </Field>
+              <Controller
+                name='label'
+                control={form.control}
+                render={({ field, fieldState }) => (
+                  <Field data-invalid={fieldState.invalid}>
+                    <FieldLabel>Label</FieldLabel>
+                    <Input
+                      {...field}
+                      aria-invalid={fieldState.invalid}
+                      placeholder='Tag Label'
+                      autoComplete='off'
+                    />
+                    {fieldState.invalid && (
+                      <FieldError errors={[fieldState.error]} />
+                    )}
+                  </Field>
+                )}
+              />
+              <Controller
+                name='value'
+                control={form.control}
+                render={({ field, fieldState }) => (
+                  <Field data-invalid={fieldState.invalid}>
+                    <FieldLabel>ID</FieldLabel>
+                    <Input
+                      {...field}
+                      aria-invalid={fieldState.invalid}
+                      placeholder='Tag ID'
+                      autoComplete='off'
+                    />
+                    {fieldState.invalid && (
+                      <FieldError errors={[fieldState.error]} />
+                    )}
+                  </Field>
+                )}
+              />
             </FieldGroup>
           </FieldSet>
           <DialogFooter>
-            <Button
-              type='submit'
-              className='cursor-pointer'>
-              Save
-            </Button>
+            <DialogClose asChild>
+              <Button
+                type='submit'
+                className='cursor-pointer'>
+                Save
+              </Button>
+            </DialogClose>
             <DialogClose asChild>
               <Button
                 variant='outline'
@@ -52,8 +110,8 @@ const NewTag = () => {
               </Button>
             </DialogClose>
           </DialogFooter>
-        </DialogContent>
-      </form>
+        </form>
+      </DialogContent>
     </Dialog>
   );
 };

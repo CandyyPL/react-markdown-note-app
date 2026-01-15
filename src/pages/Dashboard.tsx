7e56e97f9999b01da2804cd2770b3f84cd5366a2
CandyyPL <aplay58.yt@gmail.com>
2026-01-15 12:25:58 +0100

    change dashboard elements

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,13 +1,28 @@
+import NewTag from '@/components/NewTag';
 import { Button } from '@/components/ui/button';
 import { Link } from 'react-router-dom';
 
 const Dashboard = () => {
+  // const notesWithTags = useMemo(
+  //   () =>
+  //     notes.map((note) => ({
+  //       ...note,
+  //       tags: tags.filter((tag) => note.tagIds.includes(tag.id)),
+  //     })),
+  //   [notes, tags]
+  // );
+
   return (
     <main className='p-4'>
       <h1 className='mb-4 text-4xl font-semibold'>Dashboard</h1>
-      <Link to='/new'>
-        <Button className='cursor-pointer'>Create new note</Button>
-      </Link>
+      <div className='grid w-50 grid-rows-2 gap-2'>
+        <Link
+          to='/new'
+          className='w-full'>
+          <Button className='w-full cursor-pointer'>Create new Note</Button>
+        </Link>
+        <NewTag />
+      </div>
     </main>
   );
 };

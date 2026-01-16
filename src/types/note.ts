09d1e2ca0879f4ad25a3dc474c07bddcc223584a
CandyyPL <aplay58.yt@gmail.com>
2026-01-16 11:22:00 +0100

    lower required characters in body

--- src/types/note.ts
+++ src/types/note.ts
@@ -2,7 +2,7 @@ import { z } from 'zod';
 
 export const NoteDataSchema = z.object({
   title: z.string().min(3, 'Minimum 3 characters required'),
-  body: z.string().min(50, 'Minimum 50 characters required'),
+  body: z.string().min(5, 'Minimum 5 characters required'),
   tagIds: z.array(z.string()),
 });
 

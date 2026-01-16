8f16b15d05c4208405d10186dac923eca3d3ebfb
CandyyPL <aplay58.yt@gmail.com>
2026-01-16 12:39:00 +0100

    add single note page

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Dashboard from '@/pages/Dashboard';
 import NewNote from '@/pages/NewNote';
+import SingleNote from '@/pages/SingleNote';
 
 const App = () => {
   return (
@@ -13,6 +14,10 @@ const App = () => {
         <Route
           path='/new'
           element={<NewNote />}
+        />
+        <Route
+          path='/note/:id'
+          element={<SingleNote />}
         />
       </Routes>
     </Router>
---
+++ src/pages/SingleNote.tsx
@@ -0,0 +1,16 @@
+import useNotes from '@/hooks/useNotes';
+import { useParams } from 'react-router-dom';
+
+const SingleNote = () => {
+  const { id: noteId } = useParams();
+  const { notes } = useNotes();
+
+  const note = notes.find((note) => note.id === noteId);
+
+  return (
+    <main className='m-4'>
+      <h1 className='text-4xl font-semibold'>{note?.title}</h1>
+    </main>
+  );
+};
+export default SingleNote;

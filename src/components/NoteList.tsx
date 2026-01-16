4e3c48dcc9d16a90d7408cb87fff50ed46399038
CandyyPL <aplay58.yt@gmail.com>
2026-01-16 12:33:56 +0100

    made note list

---
+++ src/components/NoteList.tsx
@@ -0,0 +1,54 @@
+import { Badge } from '@/components/ui/badge';
+import { Button } from '@/components/ui/button';
+import {
+  Item,
+  ItemActions,
+  ItemContent,
+  ItemDescription,
+  ItemTitle,
+} from '@/components/ui/item';
+import useNotes from '@/hooks/useNotes';
+import useTags from '@/hooks/useTags';
+import type { Note } from '@/types/note';
+
+const NoteList = () => {
+  const { notes } = useNotes();
+  const { tags } = useTags();
+
+  return (
+    <ul className='my-4 flex flex-col gap-2'>
+      {notes.length > 0 &&
+        notes.map((note: Note) => (
+          <li
+            key={note.id}
+            className='w-200'>
+            <Item
+              variant='outline'
+              className='transition-shadow hover:shadow-md'>
+              <ItemContent>
+                <ItemTitle className='text-2xl font-semibold'>
+                  {note.title}
+                </ItemTitle>
+                <ItemDescription className='flex flex-wrap gap-1'>
+                  {note.tagIds.length > 0 &&
+                    note.tagIds.map((id) => (
+                      <Badge key={id}>
+                        {tags.find((tag) => tag.id === id)?.label}
+                      </Badge>
+                    ))}
+                </ItemDescription>
+              </ItemContent>
+              <ItemActions>
+                <Button
+                  variant='outline'
+                  className='cursor-pointer'>
+                  Preview
+                </Button>
+              </ItemActions>
+            </Item>
+          </li>
+        ))}
+    </ul>
+  );
+};
+export default NoteList;
--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,6 +1,7 @@
-import Tags from '@/components/Tags';
-import { Button } from '@/components/ui/button';
 import { Link } from 'react-router-dom';
+import { Button } from '@/components/ui/button';
+import Tags from '@/components/Tags';
+import NoteList from '@/components/NoteList';
 
 const Dashboard = () => {
   // const notesWithTags = useMemo(
@@ -14,14 +15,20 @@ const Dashboard = () => {
 
   return (
     <main className='p-4'>
-      <h1 className='mb-4 text-4xl font-semibold'>Dashboard</h1>
-      <div className='grid w-50 grid-rows-2 gap-2'>
-        <Link
-          to='/new'
-          className='w-full'>
-          <Button className='w-full cursor-pointer'>Create new Note</Button>
-        </Link>
-        <Tags />
+      <div className='flex justify-between'>
+        <h1 className='mb-4 text-4xl font-semibold'>Dashboard</h1>
+        <div className='grid grid-cols-2 gap-2'>
+          <Link
+            to='/new'
+            className='w-full'>
+            <Button className='w-full cursor-pointer'>Create new Note</Button>
+          </Link>
+          <Tags />
+        </div>
+      </div>
+      <div>
+        <h3 className='text-2xl font-semibold'>Note List</h3>
+        <NoteList />
       </div>
     </main>
   );

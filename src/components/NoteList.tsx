6e80a30efd109c820110ec702cafbb9e693b11bb
CandyyPL <aplay58.yt@gmail.com>
2026-01-19 11:20:27 +0100

    add search to note list

--- src/components/NoteList.tsx
+++ src/components/NoteList.tsx
@@ -1,3 +1,4 @@
+import NoteSearch from '@/components/NoteSearch';
 import { Badge } from '@/components/ui/badge';
 import { Button } from '@/components/ui/button';
 import {
@@ -17,41 +18,45 @@ const NoteList = () => {
   const { tags } = useTags();
 
   return (
-    <ul className='my-4 flex flex-col gap-2'>
-      {notes.length > 0 &&
-        notes.map((note: Note) => (
-          <li
-            key={note.id}
-            className='w-200'>
-            <Item
-              variant='outline'
-              className='transition-shadow hover:shadow-md'>
-              <ItemContent>
-                <ItemTitle className='text-2xl font-semibold'>
-                  {note.title}
-                </ItemTitle>
-                <ItemDescription className='flex flex-wrap gap-1'>
-                  {note.tagIds.length > 0 &&
-                    note.tagIds.map((id) => (
-                      <Badge key={id}>
-                        {tags.find((tag) => tag.id === id)?.label}
-                      </Badge>
-                    ))}
-                </ItemDescription>
-              </ItemContent>
-              <ItemActions>
-                <Link to={`/note/${note.id}`}>
-                  <Button
-                    variant='outline'
-                    className='cursor-pointer'>
-                    Preview
-                  </Button>
-                </Link>
-              </ItemActions>
-            </Item>
-          </li>
-        ))}
-    </ul>
+    <>
+      <h3 className='mb-2 text-2xl font-semibold'>Note List</h3>
+      <NoteSearch />
+      <ul className='my-4 flex flex-col gap-2'>
+        {notes.length > 0 &&
+          notes.map((note: Note) => (
+            <li
+              key={note.id}
+              className='w-200'>
+              <Item
+                variant='outline'
+                className='transition-shadow hover:shadow-md'>
+                <ItemContent>
+                  <ItemTitle className='text-2xl font-semibold'>
+                    {note.title}
+                  </ItemTitle>
+                  <ItemDescription className='flex flex-wrap gap-1'>
+                    {note.tagIds.length > 0 &&
+                      note.tagIds.map((id) => (
+                        <Badge key={id}>
+                          {tags.find((tag) => tag.id === id)?.label}
+                        </Badge>
+                      ))}
+                  </ItemDescription>
+                </ItemContent>
+                <ItemActions>
+                  <Link to={`/note/${note.id}`}>
+                    <Button
+                      variant='outline'
+                      className='cursor-pointer'>
+                      Preview
+                    </Button>
+                  </Link>
+                </ItemActions>
+              </Item>
+            </li>
+          ))}
+      </ul>
+    </>
   );
 };
 export default NoteList;

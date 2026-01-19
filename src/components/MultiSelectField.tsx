8889f993828d698faafc11dd66230ee98d6f4d93
CandyyPL <aplay58.yt@gmail.com>
2026-01-19 12:18:12 +0100

    extract multi select to external file

---
+++ src/components/MultiSelectField.tsx
@@ -0,0 +1,43 @@
+import {
+  MultiSelect,
+  MultiSelectContent,
+  MultiSelectGroup,
+  MultiSelectItem,
+  MultiSelectTrigger,
+  MultiSelectValue,
+} from '@/components/ui/multi-select';
+import useTags from '@/hooks/useTags';
+import { cn } from '@/lib/utils';
+import type { Tag } from '@/types/tag';
+
+type MultiSelectFieldProps = {
+  onChange: (values: string[]) => void;
+  className?: string | ((...inputs: string[]) => string);
+};
+
+const MultiSelectField = ({ onChange, className }: MultiSelectFieldProps) => {
+  const { tags } = useTags();
+
+  return (
+    <MultiSelect onValuesChange={onChange}>
+      <MultiSelectTrigger className={cn('cursor-pointer', className)}>
+        <MultiSelectValue
+          overflowBehavior='cutoff'
+          placeholder='Select tags...'
+        />
+      </MultiSelectTrigger>
+      <MultiSelectContent search={false}>
+        <MultiSelectGroup>
+          {tags.map((tag: Tag) => (
+            <MultiSelectItem
+              value={tag.id}
+              className='cursor-pointer'>
+              {tag.label}
+            </MultiSelectItem>
+          ))}
+        </MultiSelectGroup>
+      </MultiSelectContent>
+    </MultiSelect>
+  );
+};
+export default MultiSelectField;

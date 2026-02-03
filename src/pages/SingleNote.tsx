0256fac47b1589697695ef81aa84811db3be6a8f
CandyyPL <aplay58.yt@gmail.com>
2026-02-03 14:01:35 +0100

    add tags to single note page

--- src/pages/SingleNote.tsx
+++ src/pages/SingleNote.tsx
@@ -1,15 +1,26 @@
+import { Badge } from '@/components/ui/badge';
 import useNotes from '@/hooks/useNotes';
-import { useParams } from 'react-router-dom';
+import useTags from '@/hooks/useTags';
+import { Navigate, useParams } from 'react-router-dom';
 
 const SingleNote = () => {
   const { id: noteId } = useParams();
   const { notes } = useNotes();
+  const { tags } = useTags();
 
   const note = notes.find((note) => note.id === noteId);
 
+  if (!note) return <Navigate to='/' />;
+
   return (
     <main className='m-auto max-w-200 pt-4'>
-      <h1 className='text-4xl font-semibold'>{note?.title}</h1>
+      <h1 className='mb-4 text-4xl font-semibold'>{note?.title}</h1>
+      <span className='gap-2'>
+        {note.tagIds.length > 0 &&
+          note.tagIds.map((id) => (
+            <Badge key={id}>{tags.find((tag) => tag.id === id)?.label}</Badge>
+          ))}
+      </span>
     </main>
   );
 };

1316346ebe83875ead59f8607ce3b179b1bae0ed
CandyyPL <aplay58.yt@gmail.com>
2026-02-03 17:11:47 +0100

    remove buttons from note list item, add note open on click

--- src/components/NoteListItem.tsx
+++ src/components/NoteListItem.tsx
@@ -1,8 +1,6 @@
 import { Badge } from '@/components/ui/badge';
-import { Button } from '@/components/ui/button';
 import {
   Item,
-  ItemActions,
   ItemContent,
   ItemDescription,
   ItemTitle,
@@ -19,46 +17,27 @@ const NoteListItem = ({ note }: NoteListItemProps) => {
   const { tags } = useTags();
 
   return (
-    <li key={note.id}>
-      <Item
-        variant='outline'
-        className='h-25 w-200 transition-shadow hover:shadow-md'>
-        <ItemContent>
-          <ItemTitle className='text-2xl font-semibold'>{note.title}</ItemTitle>
-          <ItemDescription className='flex flex-wrap gap-1'>
-            {note.tagIds.length > 0 &&
-              note.tagIds.map((id) => (
-                <Badge key={id}>
-                  {tags.find((tag) => tag.id === id)?.label}
-                </Badge>
-              ))}
-          </ItemDescription>
-        </ItemContent>
-        <ItemActions>
-          <Link to={`/note/${note.id}`}>
-            <Button
-              variant='default'
-              className='cursor-pointer'>
-              Open
-            </Button>
-          </Link>
-          {/* <Link to={``}>
-            <Button
-              variant='outline'
-              className='cursor-pointer'>
-              Edit
-            </Button>
-          </Link>
-          <Link to={``}>
-            <Button
-              variant='destructive'
-              className='cursor-pointer'>
-              Delete
-            </Button>
-          </Link> */}
-        </ItemActions>
-      </Item>
-    </li>
+    <Link to={`/note/${note.id}`}>
+      <li key={note.id}>
+        <Item
+          variant='outline'
+          className='h-25 w-200 transition-shadow hover:shadow-md'>
+          <ItemContent>
+            <ItemTitle className='text-2xl font-semibold'>
+              {note.title}
+            </ItemTitle>
+            <ItemDescription className='flex flex-wrap gap-1'>
+              {note.tagIds.length > 0 &&
+                note.tagIds.map((id) => (
+                  <Badge key={id}>
+                    {tags.find((tag) => tag.id === id)?.label}
+                  </Badge>
+                ))}
+            </ItemDescription>
+          </ItemContent>
+        </Item>
+      </li>
+    </Link>
   );
 };
 export default NoteListItem;

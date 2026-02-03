7d8f0efebca77e2996fb2f36c35748868184e7a7
CandyyPL <aplay58.yt@gmail.com>
2026-02-03 17:10:17 +0100

    add note management buttons to single note page

--- src/pages/SingleNote.tsx
+++ src/pages/SingleNote.tsx
@@ -1,7 +1,8 @@
 import { Badge } from '@/components/ui/badge';
+import { Button } from '@/components/ui/button';
 import useNotes from '@/hooks/useNotes';
 import useTags from '@/hooks/useTags';
-import { Navigate, useParams } from 'react-router-dom';
+import { Link, Navigate, useParams } from 'react-router-dom';
 
 const SingleNote = () => {
   const { id: noteId } = useParams();
@@ -14,7 +15,30 @@ const SingleNote = () => {
 
   return (
     <main className='m-auto max-w-200 pt-4'>
-      <h1 className='mb-4 text-4xl font-semibold'>{note?.title}</h1>
+      <div className='flex justify-between'>
+        <h1 className='mb-4 grow-3 text-4xl font-semibold'>{note?.title}</h1>
+        <div className='flex gap-2'>
+          <Link to={`/note/${noteId}/edit`}>
+            <Button
+              variant='default'
+              className='cursor-pointer'>
+              Edit
+            </Button>
+          </Link>
+          <Button
+            variant='destructive'
+            className='cursor-pointer'>
+            Delete
+          </Button>
+          <Link to='..'>
+            <Button
+              variant='outline'
+              className='cursor-pointer'>
+              Back
+            </Button>
+          </Link>
+        </div>
+      </div>
       <span className='gap-2'>
         {note.tagIds.length > 0 &&
           note.tagIds.map((id) => (

0e42340eaba7e4bf9a515ac39b1dc4c26944f749
CandyyPL <aplay58.yt@gmail.com>
2026-03-17 13:50:54 +0100

    fixed repository annotations

--- backend/src/main/java/com/candyy/backend/domain/repositories/NoteRepository.java
+++ backend/src/main/java/com/candyy/backend/domain/repositories/NoteRepository.java
@@ -3,9 +3,11 @@ package com.candyy.backend.domain.repositories;
 import com.candyy.backend.domain.entities.NoteEntity;
 import org.springframework.data.repository.ListCrudRepository;
 import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
 
+@Repository
 public interface NoteRepository extends ListCrudRepository<NoteEntity, UUID>,
         PagingAndSortingRepository<NoteEntity, UUID> {
 }
--- backend/src/main/java/com/candyy/backend/domain/repositories/TagRepository.java
+++ backend/src/main/java/com/candyy/backend/domain/repositories/TagRepository.java
@@ -3,9 +3,11 @@ package com.candyy.backend.domain.repositories;
 import com.candyy.backend.domain.entities.TagEntity;
 import org.springframework.data.repository.ListCrudRepository;
 import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
 
+@Repository
 public interface TagRepository extends ListCrudRepository<TagEntity, UUID>,
         PagingAndSortingRepository<TagEntity, UUID> {
 }

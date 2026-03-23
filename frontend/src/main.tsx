e77bccb4a90a8b9136cc1a7d824f28a5950b830d
CandyyPL <aplay58.yt@gmail.com>
2026-03-23 14:56:58 +0100

    add react query client & provider

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,9 +2,10 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import WebFont from 'webfontloader';
 import '@/assets/global.css';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import NotesProvider from '@/context/NotesProvider.tsx';
-import App from '@/App.tsx';
 import TagsProvider from '@/context/TagsProvider.tsx';
+import App from '@/App.tsx';
 
 WebFont.load({
   google: {
@@ -12,12 +13,16 @@ WebFont.load({
   },
 });
 
+const queryClient = new QueryClient();
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <NotesProvider>
-      <TagsProvider>
-        <App />
-      </TagsProvider>
-    </NotesProvider>
+    <QueryClientProvider client={queryClient}>
+      <NotesProvider>
+        <TagsProvider>
+          <App />
+        </TagsProvider>
+      </NotesProvider>
+    </QueryClientProvider>
   </StrictMode>
 );

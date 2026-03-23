c46d1b5b7e37eb966a52e974a239258b486f5c7e
CandyyPL <aplay58.yt@gmail.com>
2026-03-23 21:55:05 +0100

    add formatDatetime utility function

--- frontend/src/lib/utils.ts
+++ frontend/src/lib/utils.ts
@@ -5,4 +5,18 @@ export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
 
+export const formatDatetime = (dateString: string): string => {
+  const d = new Date(dateString);
+  const locale = 'en-US';
+
+  const weekday = d.toLocaleDateString(locale, { weekday: 'long' });
+  const day = d.getDate();
+  const month = d.toLocaleDateString(locale, { month: 'short' });
+
+  const hrs = String(d.getHours()).padStart(2, '0');
+  const mins = String(d.getMinutes()).padStart(2, '0');
+
+  return `${weekday}, ${day} ${month} ${hrs}:${mins}`;
+};
+
 export const BACKEND_URL = 'http://localhost:8080';
